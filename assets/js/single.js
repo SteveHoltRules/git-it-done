32767cbad5305d44b106f2c113d1308dc2d80c32
Matt Luna <mat.c.luna@gmail.com>
2021-07-30 01:12:40 -0500

    Over 30 Issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -9,34 +9,52 @@ var repo = "SteveHoltRules/git-it-done"
 var apiUrl = `https://api.github.com/repos/${repo}/issues?direction=asc`;
 console.log(apiUrl);
 
-// https://api.github.com/users/SteveHoltRules/repos
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function(repo) {
+  //add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See more Issues on GitHub.com";
+  linkEl.setAttribute("href", `https://github.com/${repo}/issues`);
+  linkEl.setAttribute("target", "_blank");
+
+  //append to warning container
+  limitWarningEl.appendChild(linkEl);
+}
+
+
 
 var getRepoIssues = function (repo) {
   console.log(repo);
-  displayIssues(data);
+
+  fetch(apiUrl).then(function (response) {
+    //request was successful
+    if (response.ok) {
+      response.json().then(function (data) {
+        //pass response data to dom function
+        displayIssues(data);
+        console.log(data);
+        //check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
+      });
+    }
+    else {
+      alert("There was a problem with your request!");
+    }
+  });
 };
 
-fetch(apiUrl).then(function(response) {
-  //request was successful
-  if (response.ok) {
-    response.json().then(function(data) {
-      //pass response data to dom function
-      displayIssues(data);
-      console.log(data);
-    });
-  }
-  else {
-    alert("There was a problem with your request!");
-  }
-});
-
-var displayIssues= function(issues) {
+var displayIssues = function (issues) {
   if (issues.length === 0) {
     issueContainerEl.textContent = "This repo has no open issues!";
     return;
   }
 
-  for (var i=0; i<issues.length; i++) {
+  for (var i = 0; i < issues.length; i++) {
     //create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
